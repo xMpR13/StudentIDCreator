69e0c5116952c551281a3eb133071ff0ce9c2323
Prosper <126524261+xMpR13@users.noreply.github.com>
2024-09-14 20:31:25 +0200

    updates

--- src/main/java/prospermpuru/studentregistrationapp/Logic.java
+++ src/main/java/prospermpuru/studentregistrationapp/Logic.java
@@ -1,6 +1,9 @@
 package prospermpuru.studentregistrationapp;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Logic {
